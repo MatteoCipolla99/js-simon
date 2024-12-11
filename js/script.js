00c19e9f77f3627f4607322a8d9eb96ffe07744a
Matteo Cipolla <mati_teo@live.it>
2024-12-11 17:44:35 +0100

    feat:add html e css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const timerElm = document.getElementById('timer');
+const startElm = document.getElementById('start')
+const randomNumbersELm = document.getElementById('random-numbers')
+const userAnswerELm = document.getElementById('answer')
+const confirmElm = document.getElementById('confirm')
+const getResultElm = document.getElementById('result')
+const resetElm = document.getElementById('reset')
+
+
+
